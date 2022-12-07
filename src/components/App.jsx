d7fc789c7f5d27e56ec68b46270e0177e2377065
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-12-07 02:16:25 +0200

    api + some fncs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { getPictures } from '../helpers/Pixabay';
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+// async function fetchPics(key) {
+//   const images = await getPictures(key);
+//   console.log(images);
+// }
+
+export class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    isLoading: false,
+  };
+ 
+
+  componentDidMount() {}
+  // componentDidUpdate(_, prevState) {
+  //   if (prevState.query !== this.state.query) {
+  //     this.setState({ page: 1 });
+  //   }
+  // }
+
+  onSubmit = evt => {
+    console.log(evt);
+  };
+
+  render() {
+    return <Searchbar onSubmit={this.onSubmit}></Searchbar>;
+  }
+}
